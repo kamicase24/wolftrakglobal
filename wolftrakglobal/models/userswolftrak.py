b9368655d56cab81b66146add967ae8e50792476
Jesus Rojas <jrojas@wolftrakglobal.com>
2016-05-03 15:05:42 -0400

    Creada funcion que compara el documento de identificacion insertado con los existentes en la base de dato.

--- wolftrakglobal/models/userswolftrak.py
+++ wolftrakglobal/models/userswolftrak.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields
+from openerp import models, fields, api
 from openerp.osv import orm
+from openerp.tools.translate import _
 from openerp.osv import osv, fields
 class wolftrak_new2(osv.Model):
 	_name = "res.partner"
 	_inherit = "res.partner"
 
 	_columns = {
-		'ci': fields.char(string='CI/RNC', help='Documento de Identificacion')
+		'ci': fields.char(string='Documento de Identificacion', help='Documento de Identificacion')
 	}
+
+	@api.onchange('ci', 'phone')
+	def user_validation(self):
+		# Realiza la comparacion del dato que se insertara con los registrados en la base de datos
+		db_ci = self.search([('ci', '=', self.ci)])
+		# Almacena el campo que se insertara en variables
+		act_ci = self.ci
+		# si db_ci esta vacio(False), el usuario no esta registrado 
+		if db_ci and act_ci :
+			raise orm.except_orm(
+                _("Warning"),
+                _("""\
+Este Usuario ya esta registrado"""),
+            )
