d25b59c55c9be5f92dfc5f685e493a1c4e048e92
kamicase24 <kamicase24@hotmail.com>
2016-10-26 15:18:27 -0400

    paginacion para el modulo de paginas amarillas

--- wolftrakglobal/models/pagwolftrak.py
+++ wolftrakglobal/models/pagwolftrak.py
@@ -25,29 +25,30 @@ class pag_wolftrak(models.Model):
 		keyword = self.busc
 		if type(keyword) != bool:
 			keyword = keyword.replace(' ','-')
-			page = requests.get('http://www.paginasamarillas.com.do/Searchpg.aspx?nombre='+keyword+'&ciudad=Santo+Domingo&showQuantity=100&order=Relevancia&currentPage=1&classCode=&subClassCode=&keyword=&other=&latitude=&longitude=&filter=true')
-			content = page.content
-			soup = BeautifulSoup(content, 'lxml')
-			s_body = soup.body
-			s_body_span = s_body.find_all('span')
-			data = [] 
-			sdata = '' 
-			i=0
-			for span in s_body_span:
-				if span.parent.name == 'a' and span.attrs and span.attrs != {'class': ['search-cat']} and span.attrs != {'itemprop': 'streetAddress'} and span.attrs != {'itemprop': 'addressLocality'} and span.string != ', ':
-					sdata = sdata+span.string+',,'
-					data.append(span.string)
-
-			meta=[]
-			for n in range(len(data)):
-				if n%2 == 0:
-					meta.append(data[n:n+2])
-
+			k = 1
 			emplist = []
 			tlflist = []
-			for n in meta:
-				emplist.append(n[0])
-				tlflist.append(n[1])
+			while k <= 5:
+				j = str(k)
+				link = 'http://www.paginasamarillas.com.do/Searchpg.aspx?nombre='+keyword+'&ciudad=Santo+Domingo&showQuantity=100&order=Relevancia&currentPage='+j+'&classCode=&subClassCode=&keyword=&other=&latitude=&longitude=&filter=true'
+				print link
+				page = requests.get(link)
+				content = page.content
+				soup = BeautifulSoup(content, 'lxml')
+				s_body = soup.body
+				s_body_span = s_body.find_all('span')
+				data = [] 
+				for span in s_body_span:
+					if span.parent.name == 'a' and span.attrs and span.attrs != {'class': ['search-cat']} and span.attrs != {'itemprop': 'streetAddress'} and span.attrs != {'itemprop': 'addressLocality'} and span.string != ', ':
+						data.append(span.string)
+				meta=[]
+				for n in range(len(data)):
+					if n%2 == 0:
+						meta.append(data[n:n+2])
+				for n in meta:
+					emplist.append(n[0])
+					tlflist.append(n[1])
+				k+=1
 			# funciona...
 			mayorlist = []
 			for n in emplist:
