9cdc94d21c07a9924f31bd919b9a5102da232403
Jesus Rojas <kamicase24@hotmail.com>
2017-02-07 08:49:00 -0400

    aplicados el CSS y el header personalizado para los reportes

--- wolftrakglobal/__openerp__.py
+++ wolftrakglobal/__openerp__.py
@@ -28,11 +28,11 @@
         # 'views/609_view.xml',
         # 'views/609.xml',
         # reports
-        # 'report/wolftrak_report_header.xml',
-        # 'report/wolftrak_report_footer.xml', dejar comentado 4 ever
         # 'report/wolfttrak_report_payroll.xml',
-        'report/wolftrak_report_invoice.xml', # factura
+        # 'report/wolftrak_report_footer.xml', dejar comentado 4 ever
         # 'report/wolftrak_report_overdue.xml', # pagos pendientes / estado de cuenta temporal
+        'report/wolftrak_report_header.xml',
+        'report/wolftrak_report_invoice.xml', # factura
         'report/wolftrak_report_saleorder.xml', # presupuesto
         # views
         'views/gamification_badge_wolftrak.xml',
@@ -45,7 +45,7 @@
         # 'views/wolftrak_act_det_view.xml',
         # 'views/wolftrak_cesta_ticket.xml',
         # 'views/wolftrak_act_det_report.xml',
-        # 'views/layouts.xml',
+        'views/layouts.xml',
         # data
         'data/base_action_wolftrak.xml',
         # wizards
