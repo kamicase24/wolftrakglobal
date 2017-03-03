ff5985af1795dc009dcda9ca404de109db219136
Jesus Rojas <kamicase24@hotmail.com>
2017-03-03 09:42:35 -0400

    comentado en el modelo de la tasa monetaria, metodo de tasa default

--- wolftrakglobal/models/wolftrak_res_currency_rate.py
+++ wolftrakglobal/models/wolftrak_res_currency_rate.py
@@ -9,18 +9,18 @@ class WolftrakCurrencyRate(models.Model):
 	_inherit = "res.currency.rate"
 
 	def default_ex_rate(self):
-		page = requests.get('http://promerica.com.do/?d=1014')
-		soup = BeautifulSoup(page.content, 'lxml')
-		form = soup.body
-		result = form.find_all(href='http://www.promerica.com.do/?p=1014')
-		link = result[0]
-		str_final = link.string
-		venta = str_final[str_final.find('V'):].encode('utf-8')
-		rate = float(venta[venta.find('$') + 1:venta.find('$') + 6])
-		user = self.env.user
-		if user.company_id.name == 'Mytraktech':
-			return rate
-		else:
-			return 0.0
+		# page = requests.get('http://promerica.com.do/?d=1014')
+		# soup = BeautifulSoup(page.content, 'lxml')
+		# form = soup.body
+		# result = form.find_all(href='http://www.promerica.com.do/?p=1014')
+		# link = result[0]
+		# str_final = link.string
+		# venta = str_final[str_final.find('V'):].encode('utf-8')
+		# rate = float(venta[venta.find('$') + 1:venta.find('$') + 6])
+		# user = self.env.user
+		# if user.company_id.name == 'Mytraktech':
+		# 	return rate
+		# else:
+		return 0.0
 
 	rate = fields.Float(default=default_ex_rate)
