3b494f5bf986c444dc601a53a7296f88c2ee2f84
Jesus Rojas <jora2415@gmail.com>
2017-07-10 11:26:18 -0400

    Nuevo reporte de lista de nomina (sin finalizar)

--- models/wolftrak_payroll_report.py
+++ models/wolftrak_payroll_report.py
@@ -2,10 +2,12 @@
 import time
 import logging
 from datetime import datetime
+
 from dateutil import relativedelta
 from odoo import api, fields, models, tools, _
 from odoo.exceptions import ValidationError, UserError
 _logger = logging.getLogger(__name__)
+
 class WolftrakPayrollReport(models.Model):
     _name = 'hr.payroll.report'
     _description = 'Payroll biweekly report'
@@ -45,7 +47,28 @@ class WolftrakPayrollReport(models.Model):
                   emp_charge,
                   basic_wage,
                   daily_wage,
-                  hour_value)
+                  hour_value,
+                  worked_days,
+                  worked_days_total,
+                  no_worked_days,
+                  no_worked_days_total,
+                  d_ext_hrs,
+                  n_ext_hrs,
+                  total_d_ext_hrs,
+                  total_n_ext_hrs,
+                  holydays_sundays,
+                  total_hds_sds,
+                  bonus_days,
+                  subtotal_asg,
+                  tax1,
+                  unpaid_hours,
+                  total_unpaid_hours,
+                  tax2,
+                  loan,
+                  tax3,
+                  tax4,
+                  subtotal_dec,
+                  net)
                   
                   SELECT
                   emp.name_related,
@@ -53,8 +76,29 @@ class WolftrakPayrollReport(models.Model):
                   (SELECT name FROM hr_job WHERE id = emp.job_id),
                   (SELECT wage FROM hr_contract WHERE id = ps.contract_id),
                   (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'SD'),
-                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'VHT')
-
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'VHT'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'DLQ'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'SB'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'DD'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'TDD'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'HED'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'HEN'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'THED'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'THEN'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'DFL'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'TDFL'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'BDB'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'SubAsg'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'IVSS'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'HNR'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'THNR'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'PF'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'PRE'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'OD'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'LPH'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'SubDed'),
+                  (SELECT amount FROM hr_payslip_line WHERE slip_id = ps.id AND code = 'NET')
+                  
                   FROM hr_employee emp, hr_payslip ps 
                   WHERE emp.id = %s AND ps.id = %s''' % (emp.id, ps_id)
             _logger.info(sql)
@@ -84,6 +128,30 @@ class WolftrakPayrollReportWizard(models.TransientModel):
         return self.env['hr.payroll.report.list'].search([])
     report_list_id = fields.Many2many('hr.payroll.report.list', default=_default)
 
+    # @api.multi
+    # def print_report(self):
+    #
+    #     datas = {
+    #         'ids' : self.ids,
+    #         'model' : 'hr.payroll.report.list',
+    #         'form' : self.read()[0]
+    #     }
+    #     _logger.info('DATAS!')
+    #     _logger.info(datas)
+    #
+    #     x = {
+    #         'type' : 'ir.action.report.xml',
+    #         'report_name' : 'template_report_payroll_list',
+    #         'datas' : datas
+    #     }
+    #     _logger.info('SELF')
+    #     _logger.info(self)
+    #     _logger.info('READ!')
+    #     _logger.info(self.read)
+    #     _logger.info('RESULT!')
+    #     _logger.info(x)
+    #     return x
+
 class WolftrakPayrollList(models.Model):
     _name = 'hr.payroll.report.list'
 
@@ -93,3 +161,29 @@ class WolftrakPayrollList(models.Model):
     basic_wage = fields.Char(string='Sueldo base')
     daily_wage = fields.Char(string='Sueldo diario')
     hour_value = fields.Char(string='Valor hora')
+    worked_days = fields.Char(string='Días Trabajados')
+    worked_days_total = fields.Char(string='Total Días Trab.')
+    no_worked_days = fields.Char(string='Días Descanso')
+    no_worked_days_total = fields.Char(string='Total Días Desc.')
+    d_ext_hrs = fields.Char(string="Horas Ext. Diur.")
+    n_ext_hrs = fields.Char(string="Horas Ext. Noc.")
+    total_d_ext_hrs = fields.Char(string="Total Horas Diur.")
+    total_n_ext_hrs = fields.Char(string="Total Horas Noc.")
+    holydays_sundays = fields.Char(string="Domingos y Feriados")
+    total_hds_sds = fields.Char(string="Total por Dom. y Feriados")
+    bonus_days = fields.Char(string="Bono Día de Beneficio")
+    subtotal_asg = fields.Char(string="SubTotal de Asignaciones")
+
+    tax1 = fields.Char(string="IVSS")
+    unpaid_hours = fields.Char(string="Horas no Remuneradas")
+    total_unpaid_hours = fields.Char(string="Total Hrs. no Remuneradas")
+    tax2 = fields.Char(string="Paro Forzoso")
+    loan = fields.Char(string="Prestamo")
+    tax3 = fields.Char(string="Deduc. Atrasadas")
+    tax4 = fields.Char(string="Ley de Politica Hab.")
+    subtotal_dec = fields.Char(string="Subtotal Deducciones")
+    net = fields.Char(string="Total")
+
+
+
+
