202de90315244f6dd97a11da6ed1395e9393ed0a
Jesus Rojas <jora2415@gmail.com>
2017-07-10 13:55:30 -0400

    Finalizado boton que actualiza los datos de las oportunidades, aspecto mejorado y locación colocada en la parte principal de la vista

--- models/wolftrak_crm_lead.py
+++ models/wolftrak_crm_lead.py
@@ -1,14 +1,35 @@
 # -*- coding: utf-8 -*-
-
+import logging
 from odoo import api, fields, models, tools, SUPERUSER_ID
 from odoo.exceptions import UserError, AccessError
+_logger = logging.getLogger(__name__)
 
 
 class LeadWolftrak(models.Model):
     _inherit = "crm.lead"
 
     def update_fields(self):
+
+        _logger.info(self.partner_id.street)
+        _logger.info(self.street)
+        _logger.info(self.partner_id.street2)
+        _logger.info(self.street2)
+        _logger.info(self.partner_id.city)
+
+
         if self.partner_id.email:
             self.email_from = self.partner_id.email
         if self.partner_id.phone:
             self.phone = self.partner_id.phone
+        if self.partner_id.street:
+            self.street = self.partner_id.street
+        if self.partner_id.street2:
+            self.street2 = self.partner_id.street2
+        if self.partner_id.city:
+            self.city = self.partner_id.city
+        if self.partner_id.state_id:
+            self.state_id = self.partner_id.state_id
+        if self.partner_id.zip:
+            self.zip = self.partner_id.zip
+        if self.partner_id.country_id:
+            self.country_id = self.partner_id.country_id
