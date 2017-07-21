93880d981cd6c6437ca38f0c37d2bccf28ecec14
Jesus Rojas <jora2415@gmail.com>
2017-07-21 16:38:38 -0400

    Boton de dispositivos y vista/reporte para dispositivos creada desde el formulario de cliente (res partner)

--- models/wolftrak_res_partner.py
+++ models/wolftrak_res_partner.py
@@ -12,11 +12,13 @@ main_base = os.path.dirname(os.path.abspath(__file__))
 CONFIG_FILE_NAME = "config.json"      
 CONFIG_FILE = os.path.join(main_base, CONFIG_FILE_NAME)
 
+
 def load_config(json_file):
     with open(json_file, 'r') as file:
         config_data = json.load(file)
     return config_data
-  
+
+
 def get_rnc_record(rnc, config_data = None):
 
     if not config_data:
@@ -44,18 +46,27 @@ class WolftrakPartner(models.Model):
     _name = "res.partner"
     _inherit = "res.partner"
 
-    # @api.depends('total_device')
-    # def _set_total_devices(self):
-    #
-    #     partner_invs = self.env['account.invoice'].search([('partner_id','=',self.id)])
-    #     _logger.info(self.env.context)
-    #     devices = 0
-    #     for inv in partner_invs:
-    #         for lines in inv.invoice_line_ids:
-    #             if lines.product_id.name == 'Dispositivo GPS':
-    #                 devices = + 1
-    #     # self.total_device = devices
-    #     return devices
+    @api.multi
+    def _total_device(self):
+        invoices = self.env['account.invoice']
+
+        all_partners_and_children = {}
+        all_partner_ids = []
+        for partner in self:
+            # price_total is in the company currency
+            all_partners_and_children[partner] = self.search([('id', 'child_of', partner.id)]).ids
+            all_partner_ids += all_partners_and_children[partner]
+        _logger.info('all partners andd children')
+        _logger.info(all_partners_and_children.items())
+        _logger.info('al partners ids')
+        _logger.info(all_partner_ids)
+        # for inv in invoices.search([('partner_id', '=', self.id)], limit=1):
+        #     _logger.info(inv)
+        # self.total_device = 4
+
+        for partner, child_ids in all_partners_and_children.items():
+            partner.total_device = 0
+        return False
 
     def _default_user_id(self):
         return self.env.uid
@@ -65,7 +76,7 @@ class WolftrakPartner(models.Model):
     pay_reg = fields.Char(string='Regimen de Pago')
     doc_ident_type = fields.Integer(string='Tipo de Documento')
     user_id = fields.Many2one('res.users', string='Comercial', default=_default_user_id)
-    total_device = fields.Integer(string='Dispositivos', help='Total de dispositivos vendidos a este cliente.')
+    total_device = fields.Integer(string='Dispositivos', help='Total de dispositivos vendidos a este cliente', compute=_total_device)
 
     def _get_partner_invoices(self):
         invoices = self.env['account.invoice']
@@ -76,42 +87,47 @@ class WolftrakPartner(models.Model):
         invoices = self.env['account.invoice']
         par_inv = invoices.search([])
         for partner in self:
-            partner.partner_inv += par_inv.search([('partner_id','=',partner.id)])
+            partner.partner_inv += par_inv.search([('partner_id', '=', partner.id)])
 
     partner_inv = fields.Many2many('account.invoice', default=_get_partner_invoices, compute=_get_invoices)
 
-    @api.onchange('doc_ident','phone')
+    @api.onchange('doc_ident', 'phone')
     def user_validation(self):
 
         if self.doc_ident:
-            if len(self.doc_ident) == 11: self.doc_ident_type = 2
-            elif len(self.doc_ident) == 9: self.doc_ident_type = 1
+            if len(self.doc_ident) == 11:
+                self.doc_ident_type = 2
+            elif len(self.doc_ident) == 9:
+                self.doc_ident_type = 1
 
         db_doc_ident = self.search([('doc_ident', '=', self.doc_ident)])
         if db_doc_ident and self.doc_ident:
-            raise ValidationError(_('Este Cliente ya se encuentra registrado'))
             self.doc_ident = ''
-        db_user_client = self.search([('phone','=',self.phone)])
+            raise ValidationError(_('Este Cliente ya se encuentra registrado'))
+        db_user_client = self.search([('phone', '=', self.phone)])
         if db_user_client and self.phone:
-            raise  ValidationError(_('Este Cliente ya se encuentra registrado'))
             self.phone = ''
+            raise ValidationError(_('Este Cliente ya se encuentra registrado'))
         try:
             rnc_record = get_rnc_record(self.doc_ident)
             self.name = rnc_record[1]
             self.dgii_state = rnc_record[5]
             self.pay_reg = rnc_record[4]
-        except :
+        except:
             pass
-    #
-    # def device_history(self):
-    #     partner_invs = self.env['account.invoice'].search([('partner_id','=',self.id)])
-    #     _logger.info(self.env.context)
-    #     devices = 0
-    #     for inv in partner_invs:
-    #         for lines in inv.invoice_line_ids:
-    #             if lines.product_id.name == 'Dispositivo GPS':
-    #                 devices = + 1
-    #
-    #     _logger.info("Dispositivos: ")
-    #     _logger.info(devices)
-    #     return False
+
+    def device_history(self):
+        invoices = self.env['account.invoice']
+        partner_invoices = invoices.search([('partner_id', '=', self.id)])
+        for invoice in partner_invoices:
+            for line in invoice.invoice_line_ids:
+                _logger.info(line.name)
+
+        _logger.info("Dispositivos: ")
+        _logger.info(partner_invoices)
+
+        action = self.env.ref('wolftrakglobal.action_invoice_lines_partner')
+        result = action.read()[0]
+        # result['domain'] = [('partner_id', 'in', self.ids), ('product_id', 'in', [2, 4])]
+        result['domain'] = [('partner_id', 'in', self.ids)]
+        return result
