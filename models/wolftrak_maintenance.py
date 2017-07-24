473791734e68ae19f2efb3e04d48708aaa097083
Jesus Rojas <jora2415@gmail.com>
2017-07-24 13:46:41 -0400

    Agregadas nuevas caracteristicas para el formulario de GPS y vehiculos. Cliente, agregado estado, chasis, placas, ficha, marca y modelo

--- models/wolftrak_maintenance.py
+++ models/wolftrak_maintenance.py
@@ -4,17 +4,20 @@ from odoo.modules import get_module_resource
 import logging
 _logger = logging.getLogger(__name__)
 
+
 class MaintenanceEquipment(models.Model):
     _inherit = 'maintenance.equipment'
 
     os = fields.Char(string='Sistema Operativo')
     cpu = fields.Char(string='Procesador')
-    ram_1 = fields.Many2one('memory.storage', string='Memoria RAM', domain=[('type','=','ram')])
-    ram_2 = fields.Many2one('memory.storage', string='Memoria RAM', domain=[('type','=','ram')])
-    hard_disc = fields.Many2one('memory.storage', string='Disco Duro', domain=[('type','=','hard_disc')])
-    charger = fields.Many2one('equipment.replacement', string='Cargador', domain=[('category','=','charger')])
-    batery = fields.Many2one('equipment.replacement', string='Baterial', domain=[('category','=','batery')])
-    peripheral = fields.Many2many('equipment.replacement', string='Perifericos', domain=[('category','=','peripheral')])
+    ram_1 = fields.Many2one('memory.storage', string='Memoria RAM', domain=[('type', '=', 'ram')])
+    ram_2 = fields.Many2one('memory.storage', string='Memoria RAM', domain=[('type', '=', 'ram')])
+    hard_disc = fields.Many2one('memory.storage', string='Disco Duro', domain=[('type', '=', 'hard_disc')])
+    charger = fields.Many2one('equipment.replacement', string='Cargador', domain=[('category', '=', 'charger')])
+    batery = fields.Many2one('equipment.replacement', string='Baterial', domain=[('category', '=', 'batery')])
+    peripheral = fields.Many2many('equipment.replacement', string='Perifericos',
+                                  domain=[('category', '=', 'peripheral')])
+
 
 class MemoryStorage(models.Model):
     _name = 'memory.storage'
@@ -24,7 +27,8 @@ class MemoryStorage(models.Model):
     model_id = fields.Char(string='Modelo')
     memory = fields.Integer(string='Capacidad')
     serial = fields.Char(string='Serial')
-    type = fields.Selection([('hard_disc','Disco Duro'),('ram','Memoria RAM')], string="Tipo", required=True)
+    type = fields.Selection([('hard_disc', 'Disco Duro'), ('ram', 'Memoria RAM')], string="Tipo", required=True)
+
 
 class EquipmentReplacement(models.Model):
     _name = 'equipment.replacement'
@@ -34,7 +38,10 @@ class EquipmentReplacement(models.Model):
     model_id = fields.Char(string='Modelo')
     serial = fields.Char(string='Serial')
     note = fields.Text(string='Nota')
-    category = fields.Selection([('charger','Cargador'),('batery','Bateria'),('peripheral','Periferico')],string='Categoria', required=True)
+    category = fields.Selection([('charger', 'Cargador'),
+                                 ('batery', 'Bateria'),
+                                 ('peripheral', 'Periferico')], string='Categoria', required=True)
+
 
 class GpsDevice(models.Model):
     _name = 'gps.device'
@@ -43,7 +50,7 @@ class GpsDevice(models.Model):
     def _get_default_image(self):
         colorize, img_path, image = False, False, False
         if not image:
-            img_path = get_module_resource('wolftrakglobal','static/src/img','gps.png')
+            img_path = get_module_resource('wolftrakglobal', 'static/src/img', 'gps.png')
             _logger.info(img_path)
             colorize = True
         if img_path:
@@ -55,28 +62,71 @@ class GpsDevice(models.Model):
 
         return tools.image_resize_image_medium(image.encode('base64'))
 
-    name = fields.Char(required=True)
+    @api.onchange('car_brand_id')
+    def get_domain_model_id(self):
+
+        return {
+            'domain': {
+                'car_model_id': [('brand_id', '=', self.car_brand_id.id)]
+            }
+        }
+
+    name = fields.Char(required=True, string='Ficha')
+    # index_car = fields.Char(string='Ficha')
     image = fields.Binary(default=_get_default_image)
     brand_id = fields.Many2one('gps.brand', string='Marca')
-    model_id = fields.Many2one('gps.model',string='Modelo')
+    model_id = fields.Many2one('gps.model', string='Modelo')
     imei = fields.Char(string='IMEI', help='International Mobile Station Equipment Identity')
     esn = fields.Char(string='ESN')
     sn = fields.Char(string='S/N')
-    stock = fields.Integer(string='Stock')
+    chassis = fields.Char(string='Chasis')
+    license_plate = fields.Char(string='Placa')
+    car_brand_id = fields.Many2one('car.brand', string='Marca de la Unidad')
+    car_model_id = fields.Many2one('car.model', string='Modelo de la unidad')
+    partner_id = fields.Many2one('res.partner', string='Cliente')
+    status = fields.Selection([('on', 'Activado'), ('off', 'Desactivado')],
+                              string='Estado', readonly=True, default='on')
     note = fields.Text(string='Nota Interna')
+    proyect = fields.Char(string='Proyecto')
+
+    def status_on(self):
+        self.status = 'on'
+
+    def status_off(self):
+        self.status = 'off'
+
 
 class GpsBrand(models.Model):
     _name = 'gps.brand'
 
     name = fields.Char(string='Marca')
     note = fields.Text(string='Nota')
-    supplier = fields.Many2one('res.partner', string='Proveedor', domain=[('supplier','=',True),('customer','=',False)])
+    supplier = fields.Many2one('res.partner', string='Proveedor',
+                               domain=[('supplier', '=', True), ('customer', '=', False)])
+
 
 class GpsModel(models.Model):
     _name = 'gps.model'
 
     name = fields.Char(string='Modelo')
     note = fields.Text(string='Nota')
+
+
+class CarBrand(models.Model):
+    _name = 'car.brand'
+
+    name = fields.Char(string='Marca')
+    note = fields.Char(string='Nota')
+    model_lines = fields.One2many('car.model', 'brand_id')
+
+
+class CarModel(models.Model):
+    _name = 'car.model'
+
+    name = fields.Char(string='Modelo')
+    note = fields.Char(string='Nota')
+    brand_id = fields.Many2one('car.brand', string='Marca')
+
 
 class MobileDevice(models.Model):
     _name = 'mobile.device'
@@ -86,7 +136,7 @@ class MobileDevice(models.Model):
 
         return {
             'domain': {
-                'model_id':[('brand_id','=',self.brand_id.id)]
+                'model_id': [('brand_id', '=', self.brand_id.id)]
             }
         }
 
@@ -97,10 +147,10 @@ class MobileDevice(models.Model):
     batery = fields.Char(string='Batería')
     batery_serial = fields.Char(string='Serial de la Batería')
     batery_model = fields.Char(string='Modelo de la Batería')
-    employee_id = fields.Many2one('hr.employee', string='Empleado', domain=[('id','!=',1)], help='Empleado Asignado a este número de telefono')
+    employee_id = fields.Many2one('hr.employee', string='Empleado', domain=[('id', '!=', 1)],
+                                  help='Empleado Asignado a este número de telefono')
     number = fields.Integer(string='Número asociado')
     simcard_imei = fields.Integer(string='IMEI de la Simcard')
-
 
 
 class MobileBrand(models.Model):
@@ -115,6 +165,7 @@ class MobileBrand(models.Model):
     #
     #     vals['name'] = self.name+' ['+self.model+']'
     #     return super(MobileBrand, self).write(vals)
+
 
 class MobileModel(models.Model):
     _name = 'mobile.model'
